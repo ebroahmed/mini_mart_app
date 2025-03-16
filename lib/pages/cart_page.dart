c9a5225c5909109622fdd31068d739beb5c10d52
ebroahmed <ebrahimahmed804853@gmail.com>
2025-03-16 16:08:48 +0300

    cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:mini_mart_app/model/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cart'),
+      ),
+      body: Consumer<CartModel>(builder: (context, value, child) {
+        return Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: value.cartItems.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(value.cartItems[index][0]),
+                      );
+                    }))
+          ],
+        );
+      }),
+    );
   }
 }
