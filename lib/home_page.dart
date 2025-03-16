451623bed7453356dc8e90b1e7a52ee0ae238f47
ebroahmed <ebrahimahmed804853@gmail.com>
2025-03-16 14:40:27 +0300

    home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Welcome!",
+                style: GoogleFonts.poppins(fontSize: 17, color: Colors.black87),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Let's order fresh fruits for you",
+                style: GoogleFonts.poppins(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Divider(),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 15),
+              child: Text(
+                'Fresh Items',
+                style: GoogleFonts.montserrat(fontSize: 15),
+              ),
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return MartItemTile();
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
